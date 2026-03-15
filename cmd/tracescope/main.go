70d740c6ddd37324c1f38c83aefba757ce0a7902
Anuragp22 <anuragp2003b@gmail.com>
2026-03-15 16:04:35 +0530

    feat: add risk-based exit codes for CI gating
    Exit 1 for HIGH risk, exit 2 for MEDIUM risk, exit 0 for LOW/none.
    Enables CI pipelines to fail builds based on blast radius severity.

--- cmd/tracescope/main.go
+++ cmd/tracescope/main.go
@@ -1,13 +1,19 @@
 package main
 
 import (
+	"errors"
 	"os"
 
+	"github.com/anurag/tracescope/internal/analyzer"
 	"github.com/anurag/tracescope/internal/cmd"
 )
 
 func main() {
 	if err := cmd.Execute(); err != nil {
+		var riskErr *analyzer.RiskExitError
+		if errors.As(err, &riskErr) {
+			os.Exit(riskErr.Code)
+		}
 		os.Exit(1)
 	}
 }
